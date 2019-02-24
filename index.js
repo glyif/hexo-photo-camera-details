4795c75f1a2c7dd725b768678411e7c72aca58c8
Bobby Yang <bobby.yang@makewonder.com>
2019-02-24 09:40:39 -0800

    update with image tags and some divs

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-hexo.extend.tag.register('detailed-photo', function(){
+hexo.extend.tag.register('photo_details_plugin', function(args){
  
-    return '<p>initial hexo-photo-details commit</p>';
+    var imageSrc = args[0];
+    return renderImage(imageSrc);
  
 });
+
+function handleInfoButtonClick(e) {
+    debugger;
+}
+
+function renderPhotoInfo() {
+
+}
+
+function renderCaption() {
+
+}
+
+function renderImage(imageSrc) {
+    let infoButtonClicked = false;
+    return `<div style="position: relative;">
+    <img src=${imageSrc}></img>
+    <button style="position: absolute; bottom: 4em; right: 5em;" onClick=${handleInfoButtonClick.bind(this)}>Info</button>
+    <div style="text-align:center">caption is here</div>
+    </div>`
+}
